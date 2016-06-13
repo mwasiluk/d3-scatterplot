50ca32808ca3432a1bbf0131c9c8913e16ce8994
mwasiluk <m.wasiluk89@gmail.com>
2016-06-13 12:46:57 +0200

    gulp build-css task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,29 @@
 var gulp = require('gulp');
+var del = require('del');
 var merge = require('merge-stream');
 var plugins = require('gulp-load-plugins')();
 
+gulp.task('clean', function (cb) {
+    return del(['tmp', 'dist'], cb);
+});
+
+gulp.task('build-css', ['clean'], function () {
+    return gulp.src('./styles/*')
+        .pipe(plugins.plumber({ errorHandler: onError }))
+        .pipe(plugins.sass())
+        .pipe(plugins.minifyCss())
+        .pipe(plugins.rename({
+            extname: '.min.css'
+        }))
+        .pipe(gulp.dest('./dist'));
+});
 
 gulp.task('default', function() {
 
 });
+
+// error function for plumber
+var onError = function (err) {
+    console.log(err);
+    this.emit('end');
+};
